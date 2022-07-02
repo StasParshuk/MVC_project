8caaa949259aa7c9810e6ae39a8b46b0d1f9168f
stas parshuk <99249013+StasParshuk@users.noreply.github.com>
2022-07-02 14:09:47 +0300

    Revert "Dz 13"

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . "/setting.php";
-use config\config;
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
-
-use core\Model;
-
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,17 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'view\\' => array($baseDir . '/app/view'),
-    'core\\' => array($baseDir . '/core'),
-    'controller\\' => array($baseDir . '/app/controller'),
-    'config\\' => array($baseDir . '/config'),
-    'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
-    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
     'Symfony\\Component\\VarDumper\\' => array($vendorDir . '/symfony/var-dumper'),
-    'PhpOption\\' => array($vendorDir . '/phpoption/phpoption/src/PhpOption'),
-    'Model\\' => array($baseDir . '/app/model'),
-    'GrahamCampbell\\ResultType\\' => array($vendorDir . '/graham-campbell/result-type/src'),
-    'Dotenv\\' => array($vendorDir . '/vlucas/phpdotenv/src'),
-    'App\\' => array($baseDir . '/app'),
 );
